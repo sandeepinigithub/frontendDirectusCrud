79c56de0c4368de444d9513eb67d69d6c9f9090f
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2022-02-26 15:30:59 +0530

    applied css and make sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontendDirectusCrud';
+  mobileView: number = 992;
+  toggle: boolean = false;
+  attachEvents() {
+    window.onresize = () => {
+      if (this.getWidth() >= this.mobileView) {
+        if (localStorage.getItem('toggle')) {
+          this.toggle = !localStorage.getItem('toggle') ? false : true;
+        } else {
+          this.toggle = true;
+        }
+      } else {
+        this.toggle = false;
+      }
+    }
+  }
+
+  getWidth() {
+    return window.innerWidth;
+  }
+
+  toggleSidebar() {
+    this.toggle = !this.toggle;
+    localStorage.setItem('toggle', this.toggle.toString());
+  }
 }
